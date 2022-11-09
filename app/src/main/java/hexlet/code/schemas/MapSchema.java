a1545716fa59774b81212753994d032027143b58
Vadim Tentyuk <vadim.tentyuk97@gmail.com>
2022-11-09 12:23:41 +1000

    1st review fixes

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -29,8 +29,7 @@ public class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
 
 
     public final MapSchema<K, V> shape(Map<String, BaseSchema> inputSchema) {
-        addConditions("shape", input -> input instanceof Map
-                && inputSchema.entrySet().stream().allMatch(e ->
+        addConditions("shape", input -> inputSchema.entrySet().stream().allMatch(e ->
                 e.getValue().isValid(((Map<?, ?>) input).get(e.getKey()))));
         return this;
     }
